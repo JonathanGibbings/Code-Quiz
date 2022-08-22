38b35aeb2422317c282a166404f47b0e66d4e03f
JGibbings <j_gibbings@msn.com>
2022-08-21 21:27:17 -0400

    start btn clears screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,58 @@
 // time limit for quiz
 var countLength = 75;
-var startBtnEl = document.querySelector(".start-btn");
+var startBtnEl = document.querySelector("#start-btn");
+var quizAreaEl = document.querySelector("#quiz-area");
+var questions = [
+    {
+        question: "Question 1 is 4?",
+        1: "answer 1",
+        2: "answer 2",
+        3: "answer 3",
+        4: "answer 4",
+        answer: 4
+    },
+    {
+        question: "Question 2 is 3?",
+        1: "answer 1",
+        2: "answer 2",
+        3: "answer 3",
+        4: "answer 4",
+        answer: 3
+    }
+]
 
-
-
+// Runs countdown till end of quizLength set at top
 var countdown = function() {
     var timerEl = document.querySelector("#timer");
     timerEl.innerHTML = "Time: " + countLength;
     setInterval(function(){
         timerEl.innerHTML = "Time: " + countLength;
         if (countLength < 1) {
-            endQuiz();
+            endQuiz(countLength);
         }
         countLength--;
     }, 1000)
 }
 
-startBtnEl.addEventListener("click", countdown);
+// Starts quiz by starting countdown, shuffling questions, and pulling first question
+var startQuiz = function() {
+    var introEl = document.querySelector("#intro-box");
+    introEl.remove();
+    countdown();
+    randomizeQuestions();
+    nextQuestion();
+}
+
+// randomizes the order of the questions in the array
+var randomizeQuestions = function() {}
+
+// puts the next question on the screen
+var nextQuestion = function() {
+
+}
+
+// brings up the highscore screen
+var endQuiz = function(score) {}
+
+// event listeners for buttons
+startBtnEl.addEventListener("click", startQuiz);
